556ab13d0f2029a2a67ef9f8706242e16b502f3e
Yogesh Jadhav <yogeshraje.jadhav@gmail.com>
2021-02-01 18:44:17 +0530

    Update nav bar

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import FootBallTeam from './components/footbTeam';
 import TennisTeam from './components/tennisTeam';
 import SwimmingTeam from './components/swimmingTeam';
 import { Navbar, Form, FormControl, Button, Nav, Link} from 'react-bootstrap';
+import homePage from './components/home';
 
 export default function App() {
   return (
     <div>
       <Router>
         <Navbar bg="primary" variant="dark">
-          <Navbar.Brand >Team Records</Navbar.Brand>
+          <Navbar.Brand href="/">Team Records</Navbar.Brand>
           <Nav className="mr-auto">
+            <Nav.Link href="/"></Nav.Link>
             <Nav.Link href="/cricket">Cricket</Nav.Link>
             <Nav.Link href="/tennis">Tennis</Nav.Link>
-            <Nav.Link href="/football">FootBall</Nav.Link>
+            <Nav.Link href="/football">Football</Nav.Link>
+            <Nav.Link href="/swimming">Swimming</Nav.Link>
           </Nav>
           <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -23,6 +26,7 @@ export default function App() {
           </Form>
         </Navbar>
         <Switch>
+          <Route path = "/" exact component = {homePage} />
           <Route path = "/cricket" component = {Cricket} />
           <Route path = "/tennis" component = {TennisTeam} />
           <Route path = "/football" component = {FootBallTeam} />
