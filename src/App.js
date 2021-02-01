269569ba99ba4d9dc311ac53a11a18b0f4617aa1
Yogesh Jadhav <yogeshraje.jadhav@gmail.com>
2021-02-01 18:31:33 +0530

    Updated navbar using react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, NavLink} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Cricket from './components/cricketTeam';
 import FootBallTeam from './components/footbTeam';
 import TennisTeam from './components/tennisTeam';
 import SwimmingTeam from './components/swimmingTeam';
+import { Navbar, Form, FormControl, Button, Nav, Link} from 'react-bootstrap';
+
 export default function App() {
   return (
     <div>
       <Router>
-        <ul>
-          <li><NavLink  to='/cricket'>Cricket Team</NavLink></li>
-          <li><NavLink  to='/tennis'>Tennis Team</NavLink></li>
-          <li><NavLink  to='/football'>FootBall Team</NavLink></li>
-          <li><NavLink  to='/swimming'>Swimming Team</NavLink></li>
-        </ul>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand >Team Records</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/cricket">Cricket</Nav.Link>
+            <Nav.Link href="/tennis">Tennis</Nav.Link>
+            <Nav.Link href="/football">FootBall</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar>
         <Switch>
           <Route path = "/cricket" component = {Cricket} />
           <Route path = "/tennis" component = {TennisTeam} />
--- src/components/swimmingTeam.js
+++ src/components/swimmingTeam.js
@@ -1,7 +1,8 @@
 import React from 'react';
-
 export default function SwimmingTeam(){
     return(
+      <div>
         <h3>Swimming Team </h3>
+      </div>
     )
 }
